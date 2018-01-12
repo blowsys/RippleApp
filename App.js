545bc9a012874c816601babeaf899355345a19a2
PhilStar <philstar@PhilStars-iMac.local>
2018-01-12 17:47:27 +0300

    state init

--- App.js
+++ App.js
@@ -7,8 +7,11 @@ import {
 	TouchableOpacity,
 	Image,
 	View,
-	Dimensions
+	Dimensions,
+	Alert
 } from 'react-native';
+import PopupDialog from 'react-native-popup-dialog';
+
 
 const PAD_WIDTH = 768;
 const PAD_HEIGHT = 1024;
@@ -39,7 +42,23 @@ const getStyle = (x, name, isIPad) => {
 }
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			cash:'5542464'
+		};
+		this.generate = () => this.setState((prev) => {return {cash : Math.floor(Math.random() * 9000000) + 1000000}});
+		// this.generate= this.generate.bind(this)
+	}
+	componentWillMount() {
+		// this.setState((prev) => {return {cash : Math.floor(Math.random() * 9000000) + 1000000}});
+	}
 	render() {
+		
+		let cash = this.state.cash
+		console.log(this.state);
+		console.log(this.generate());
+		
 		return (
 			<View style={styles.container}>
 				<Text style={[styles.title,getStyle(getRatio(), 'title', isIPad)]}>Ripple Generator</Text>
@@ -54,16 +73,31 @@ export default class App extends React.Component {
 					}}>
 					<Image source={require("./src/Images/MainCard.png")} style={[styles.MainCard,getStyle(getRatio(), 'MainCard', isIPad)]}/>
 
-					<Text style={[styles.generatedNumber,getStyle(getRatio(), 'generatedNumber', isIPad)]}>5205910</Text>
+					<Text style={[styles.generatedNumber,getStyle(getRatio(), 'generatedNumber', isIPad)]}>{this.state.cash}</Text>
 
 					<Image source={require("./src/Images/Circle1.png")} style={[styles.circle,getStyle(getRatio(), 'circle', isIPad)]}/>
 
 					<Text style={[styles.timer,getStyle(getRatio(), 'timer', isIPad)]}>0:41:10</Text>
 
 					<TouchableOpacity
+						onPress={this.generate()}
 						style={[styles.button,getStyle(getRatio(), 'button', isIPad)]}>
 							<Text style={[styles.buttonText,getStyle(getRatio(), 'buttonText', isIPad)]}>Generate</Text>
 					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={() => Alert.alert(
+							'Withdraw your cash',
+							'You have '+ cash +' XRP but for withdraw you need 1.0000000 XRP',
+							[
+								{text: 'Cancel', onPress: () => console.log('Cancel Button Pressed'), style: 'cancel'},
+						   
+								{text: 'OK', onPress: () => console.log('OK ButtonPressed')},
+							  ]
+							)}
+						style={[styles.button,getStyle(getRatio(), 'button', isIPad)]}>
+							<Text style={[styles.buttonText,getStyle(getRatio(), 'buttonText', isIPad)]}>Generate</Text>
+					</TouchableOpacity>
+
 				</View>
 			</View>
 		);
@@ -74,6 +108,7 @@ const styles =StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
+		justifyContent:'center'
 	},
 	title:{
 		color:pink,
@@ -126,7 +161,7 @@ const styles =StyleSheet.create({
 		alignItems:'center',
 		borderRadius:10,
 		width:255,
-		marginTop:170,
+		// marginTop:170,
 		height:50
 	},
 	buttonText:{
